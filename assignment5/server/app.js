8f8b507ea1f3abcb9f427c2e5f627fa6e9f6d0ba
Gavin Servai <gavinservai@gmail.com>
2015-11-29 23:58:28 -0800

    Assignment 5

---
+++ assignment5/server/app.js
@@ -0,0 +1,102 @@
+// Based on the code sample provided here: https://github.com/karthikp-ubc/CPEN400A-codesamples/blob/master/lecture8/ajaxserver.js
+
+var http = require("http");
+if (! http) process.exit(1);
+
+var fs = require("fs");
+if (! fs) process.exit(2);
+
+// MongoDB code based on official guide: https://docs.mongodb.org/getting-started/node/client/
+var MongoClient = require("mongodb").MongoClient;
+
+/**
+* Adds the order into the database
+*/
+function postOrder(cart, response) {
+
+  MongoClient.connect("mongodb://localhost:27017/cpen400a", function(err, db) {
+    if (err) throw err;
+
+    // Insert the json data for the order into the orders collection
+    var orders = db.collection("orders");
+    orders.insert(order);
+    db.close();
+
+  });
+
+  response.end();
+}
+
+function getProductsFromDb(products, response) {
+
+  var products = products;
+  MongoClient.connect("mongodb://localhost:27017/cpen400a", function(err, db) {
+    if (err) throw err;
+    // Iterate through each product, building up the products variable
+
+    db.collection('products').find().each(function(err, item) {
+      if (item) {
+        console.log('item found');
+        var productName = item['name'];
+        products[productName] = {
+          "price" : item['price'],
+          "quantity" : item['quantity'],
+          "url" : item['image']
+        };
+      }else{
+        db.close();
+        response.write(JSON.stringify(products));
+        response.statusCode = 200;
+        response.end();
+      } 
+    });
+  });
+}
+
+/**
+* Retrieves all of the products
+*/
+function getProducts(response) {
+  var products = {};
+  products = getProductsFromDb(products, response);
+}
+
+
+var serveRequest = function(request, response) {
+  // Set CORS headers
+  response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Request-Method', '*');
+  response.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET');
+  response.setHeader('Access-Control-Allow-Headers', '*');
+  if ( response.method === 'OPTIONS' ) {
+    res.writeHead(200);
+    res.end();
+    return;
+  }
+  console.log('request received');
+  if (request.url === "/products") {
+    getProducts(response);
+  } else if (request.url === "/checkout" && request.method === "POST") {
+
+    var postData = '';
+
+    // Get the POST data
+    request.on("data", function(data) {
+      postData += data;
+    });
+
+    // Add the order into the db
+    request.on("end", function() {
+      var cart = JSON.parse(postData);
+      postOrder(cart, response);
+    });
+
+  } else {
+    response.end();
+  }
+}
+
+var port = 8080;
+var server = http.createServer(serveRequest);
+server.listen(port);
+console.log("Starting server on port " + port);
---
+++ assignment5/server/initDb.js
@@ -0,0 +1,42 @@
+// Removes documents from all collections, and initializes the products
+
+var MongoClient = require("mongodb").MongoClient;
+
+MongoClient.connect("mongodb://localhost:27017/cpen400a", function(err, db) {
+    if (err) throw err;
+
+    // Delete data from all collections
+
+    var orders = db.collection("orders");
+    orders.deleteMany({}, function(err, results) {
+        console.log(results);
+    });
+
+    var users = db.collection("users");
+    users.deleteMany({}, function(err, results) {
+        console.log(results);
+    });
+
+    var products = db.collection("products");
+    products.deleteMany({}, function(err, results) {
+        console.log(results);
+    });
+
+    // Populate the products table
+    products.insert({name:"KeyboardCombo",price: "26", quantity: "1", image: "https://cpen400a.herokuapp.com/images/KeyboardCombo.png"});
+    products.insert({name:"Mice",price: "5", quantity: "8", image: "https://cpen400a.herokuapp.com/images/Mice.png"});
+    products.insert({name:"PC1",price: "313", quantity: "10", image: "https://cpen400a.herokuapp.com/images/PC1.png"});
+    products.insert({name:"PC2",price: "375", quantity: "9", image: "https://cpen400a.herokuapp.com/images/PC2.png"});
+    products.insert({name:"PC3",price: "380", quantity: "10", image: "https://cpen400a.herokuapp.com/images/PC3.png"});
+    products.insert({name:"Tent",price: "37", quantity: "0", image: "https://cpen400a.herokuapp.com/images/Tent.png"});
+    products.insert({name:"Box1",price: "6", quantity: "2", image: "https://cpen400a.herokuapp.com/images/Box1.png"});
+    products.insert({name:"Box2",price: "5", quantity: "7", image: "https://cpen400a.herokuapp.com/images/Box2.png"});
+    products.insert({name:"Clothes1",price: "26", quantity: "4", image: "https://cpen400a.herokuapp.com/images/Clothes1.png"});
+    products.insert({name:"Clothes2",price: "23", quantity: "8", image: "https://cpen400a.herokuapp.com/images/Clothes2.png"});
+    products.insert({name:"Jeans",price: "39", quantity: "8", image: "https://cpen400a.herokuapp.com/images/Jeans.png"});
+    products.insert({name:"Keyboard",price: "22", quantity: "6", image: "https://cpen400a.herokuapp.com/images/Keyboard.png"});
+
+
+    db.close();
+
+});
